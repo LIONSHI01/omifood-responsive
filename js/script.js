3bd61be22f25231b9b7f0aad0ba6f90b041f0990
LIONSHI01 <shizijian2010@yahoo.com.hk>
2022-06-15 11:53:18 +0800

    add scroll feature to links

--- js/script.js
+++ js/script.js
@@ -11,6 +11,32 @@ btnNavEl.addEventListener("click", function () {
   headerEl.classList.toggle("nav-open");
 });
 
+/////////////////////////////////////////////////////////
+//KEYNOTE:Smooth scrolling animation
+const allLinks = document.querySelectorAll("a:link");
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    // Scroll back to top
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+
+    // Scroll to selected elements
+    if (href !== "#" && href.startsWith("#")) {
+      const selectEl = document.querySelector(href);
+      selectEl.scrollIntoView({ behavior: "smooth" });
+    }
+    // Close mobile navigation
+    if (link.classList.contains("main-nav-link"))
+      headerEl.classList.toggle("nav-open");
+  });
+});
+
 ///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
 function checkFlexGap() {
